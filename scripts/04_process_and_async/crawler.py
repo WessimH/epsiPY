4f6d68843f00646d2130b8d8d72d21239c400ec1
Gregoire GUEMAS <gregoire.guemas@navispeed.eu>
2024-10-25 16:49:55 +0200

    With classes

--- scripts/04_process_and_async/crawler.py
+++ scripts/04_process_and_async/crawler.py
@@ -1,5 +1,6 @@
 import asyncio
 from dataclasses import dataclass
+from multiprocessing.pool import Pool
 from typing import Any
 
 import aiohttp
@@ -8,6 +9,7 @@ import bs4
 from bs4 import ResultSet, Tag
 from urllib.parse import urljoin
 
+
 @dataclass
 class Page:
     url: str
@@ -15,48 +17,55 @@ class Page:
     body: str
     links: list[str]
 
-async def parse(url: str, html: str) -> Page:
-    soup = bs4.BeautifulSoup(html)
-    all_links: ResultSet[Tag] = soup.find_all("a")
-    final_links: list[str] = []
 
-    link: Tag
-    for link in all_links:
-        href = link.attrs.get("href", None)
-        href = urljoin(url, href)
-        final_links.append(str(href))
+class Crawler:
 
-    return Page(url, soup.title, soup.body, final_links)
+    def __init__(self, pool: Pool):
+        self.queue = asyncio.Queue()  # Input
+        self.history = set()
 
-async def fetch(url: str) -> Page:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
+    async def parse(self, url: str, html: str) -> Page:
+        soup = bs4.BeautifulSoup(html)
+        all_links: ResultSet[Tag] = soup.find_all("a")
+        final_links: list[str] = []
 
-            html = await response.text()
-            print("Body:", html[:15], "...")
-            return await parse(url, html)
+        link: Tag
+        for link in all_links:
+            href = link.attrs.get("href", None)
+            href = urljoin(url, href)
+            final_links.append(str(href))
+
+        return Page(url, soup.title, soup.body, final_links)
+
+    async def fetch(self, url: str) -> Page:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                print("Status:", response.status)
+                print("Content-type:", response.headers['content-type'])
+
+                html = await response.text()
+                print("Body:", html[:15], "...")
+                return await self.parse(url, html)
+
+    async def worker(self):
+        while True:
+            print("Waiting for work")
+            url = await self.queue.get()
+            if url in self.history:
+                continue
+            self.history.add(url)
+            print("Work to do", url)
+            page = await self.fetch(url)
+            for link in page.links:
+                await self.queue.put(link)
 
 
-queue = asyncio.Queue() # Input
-history = set()
+async def start(pool: Pool):
+    crawler = Crawler(pool)
+    await crawler.queue.put("https://fr.wikipedia.org/")
+    await crawler.worker()
 
-async def worker():
-    while True:
-        print("Waiting for work")
-        url = await queue.get()
-        if url in history:
-            continue
-        history.add(url)
-        print("Work to do", url)
-        page = await fetch(url)
-        for link in page.links:
-            await queue.put(link)
-
-async def start():
-    await queue.put("https://fr.wikipedia.org/")
-    await worker()
 
 if __name__ == '__main__':
-    asyncio.run(start())
+    pool = Pool(4)
+    asyncio.run(start(pool))
